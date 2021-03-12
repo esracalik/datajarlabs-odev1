2850b168bc13c04c1487918e923ebc555d749eeb
esracalik <46395822+esracalik@users.noreply.github.com>
2021-03-12 23:05:09 +0300

    Ödev 1

---
+++ Esra Çalık.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+carpim=1000*12
+print("1000 ve 12 sayılarının çarpımı:",carpim)
+bolum= 12000/100
+print("12000'in 100'e bölümü:" , bolum)
+
+
+# In[17]:
+
+
+carpim=120*12
+print("120 ve 12 sayılarının çarpımı:",carpim)
+bolum=1440/100
+print("1440'ın 100'e bölümü:" , bolum)
+toplam=14.4+120
+print("14.4 ile 120 sayılarının toplamı:", toplam)
+
+
+# In[20]:
+
+
+carpim=134.4*12
+print("134.4 ve 12 sayılarının çarpımı:", carpim)
+bolum=1612.8000000000002/100
+print("1612.8000000000002'ın 100'e bölümü:" , bolum)
+toplam= 16.128+134.4
+print("16.128 ile 134.4 sayılarının toplamı:" , toplam)
+
+
+# In[23]:
+
+
+carpim=150.52800000000002*12
+print("150.52800000000002 ve 12 sayılarının çarpımı:", carpim)
+bolum=1806.3360000000002/100
+print("1806.3360000000002'ın 100'e bölümü:" , bolum)
+toplam=18.063360000000003+ 150.52800000000002
+print("18.063360000000003 ile 150.52800000000002 sayılarının toplamı:" , toplam)
+
+
+# In[26]:
+
+
+carpim=168.59136*12
+print("168.59136 ve 12 sayılarının çarpımı:" , carpim)
+bolum=2023.09632/100
+print("2023.09632'nin 100'e bölümü:" , bolum)
+toplam= 20.2309632+168.59136
+print("20.2309632 ile 168.59136 sayılarının toplamı:" , toplam)
+
+
+# In[29]:
+
+
+carpim=188.8223232*12
+print("188.8223232 ve 12 sayılarının çarpımı:" , carpim)
+bolum=2265.8678784/100
+print("2265.8678784'ün 100'e bölümü:" , bolum)
+toplam=22.658678784+188.8223232
+print("22.658678784 ile 188.8223232 sayılarının toplamı:" ,toplam)
+
+
+# In[33]:
+
+
+carpim=211.481001984*12
+print("211.48100198 ve 12 sayılarının çarpımı:" , carpim)
+bolum=2537.772023808/100
+print("2537.772023808'in 100'e bölümü:", bolum)
+toplam=25.377720238080002+211.481001984
+print("25.377720238080002 ile 211.481001984 sayılarının toplamı:" ,toplam)
+
+
+# In[ ]:
+
+
+
+Hafta başında 1000 dolarlık bitcoin aldığımızda günde ortalama %12 kazanç
+
+
+# In[40]:
+
+
+miktar=1000
+yüzde=12
+print("Hafta başında", miktar ," dolarlık bitcoin aldığımızda günde ortalama % ", yüzde , "kazanç")
+
+
+# In[ ]:
+
+
+bir hafta sonunda 1210.68 dolar kazanırdık
+
+
+# In[42]:
+
+
+dolar=1210.68
+print("bir hafta sonunda", dolar," kazanırdık ")
+
+
+# In[ ]:
+
+
+
+
